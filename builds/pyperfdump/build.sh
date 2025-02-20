fe85b09cc29df71881b3aabedef2d92a48d77856
chaseleif <chaseleif@icloud.com>
2025-02-20 14:05:43 -0600

    improve setup/build use sysctl to change perf_event_paranoid change conditions and make build.sh more standalone

--- builds/pyperfdump/build.sh
+++ builds/pyperfdump/build.sh
@@ -9,17 +9,24 @@ fi
 # Optional dependencies for variant features
 # The demo.py script always tries to import mpi4py
 # Use of HDF5 or PHDF5 depends on whether MPI is available
-mpiexec -n 1 python -c 'from mpi4py import MPI' 2>/dev/null
-havempi="$?"
-[ "$havempi" -eq 0 ] && usempi="ON" || usempi="OFF"
+if mpiexec -n 1 python -c 'from mpi4py import MPI' 2>/dev/null ; then
+  usempi="ON"
+else
+  usempi="OFF"
+fi
 # hdf5 check for the h5dump bin
-which h5dump >/dev/null 2>&1
-havehdf5="$?"
-[ "$havehdf5" -eq 0 ] && enablehdf5="ON" || enablehdf5="OFF"
+if which h5dump >/dev/null 2>&1 ; then
+  enablehdf5="ON"
+else
+  enablehdf5="OFF"
+fi
+
+# the pyperfdump directory
+[ ! -d "pyperfdump/" ] && git clone https://github.com/RECUP-DOE/pyperfdump.git
 
 # fresh build directory
 [ -d "build/" ] && rm -rf build/
-mkdir build && cd build
+mkdir build && cd build || exit 1
 cmake "-DUSE_MPI:BOOL=$usempi" \
       "-DENABLE_HDF5:BOOL=$enablehdf5" \
       "../pyperfdump/"
