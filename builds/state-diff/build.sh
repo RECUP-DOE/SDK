1fed88290b9e010f5efe42ec74af88513a7df361
10emyrt <kta7930@rit.edu>
2025-08-21 13:51:51 -0400

    adding mini tutorial for state-diff python project

--- builds/state-diff/build.sh
+++ builds/state-diff/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ ! -d "statediff-RECUP/" ] && git clone https://github.com/RECUP-DOE/statediff-RECUP.git
+[ ! -d "statediff-RECUP/" ] && git clone --recurse-submodules https://github.com/RECUP-DOE/statediff-RECUP.git
 cd statediff-RECUP
 
 ./bootstrap.sh
